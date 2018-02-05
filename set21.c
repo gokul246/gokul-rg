8be457a7e051234281597ed8a640aecc584cbb66
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-05 14:09:49 +0530

    Update and rename set110.c to set21.c

---
+++ set21.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the number");
+    scanf("%d%d",&a&b);
+    {
+        c=pow(a,b);
+    }
+    return 0;
+    )
