641c77027377c5d673b8581705a63788181907df
gokul246 <35866872+gokul246@users.noreply.github.com>
2018-02-07 14:21:35 +0530

    Create set23.c

---
+++ set23.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,i;
+printf("enter the number");
+scanf("%d%d",&a&b);
+{
+for(i=1;i=a/b;i++)
+c=a/b;
+if(i==1)
+printf("the number is prime..");
+else
+{
+printf("the number is not a prime");
+}
+getch();
+}
